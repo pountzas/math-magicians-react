003db7b87ee60b1ce51549937e0a2bb7e7e365b0
pountzas <nikos@pountzas.gr>
2021-08-18 23:09:47 +0300

    import calculator on App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 // import Navbar from './components/Navbar';
-// import Calculator from './components/Calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      {/* <Calculator /> */}
+      <Calculator />
     </div>
   );
 }
