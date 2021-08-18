208313fe59c1299f435a296c4fdb0facba6a38f5
pountzas <nikos@pountzas.gr>
2021-08-18 23:38:51 +0300

    fix linter problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './App.css';
-// import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+// import React, { Component } from 'react';
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>hello</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Navbar;
