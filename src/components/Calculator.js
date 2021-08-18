0f82fda3530cd911956849b54352c840a6084e53
pountzas <nikos@pountzas.gr>
2021-08-18 23:09:15 +0300

    add extra classes for orange keys and zero

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,22 +19,22 @@ class Calculator extends React.Component {
           <div className="key">AC</div>
           <div className="key">+/-</div>
           <div className="key">%</div>
-          <div className="key">÷</div>
+          <div className="key orange">÷</div>
           <div className="key">7</div>
           <div className="key">8</div>
           <div className="key">9</div>
-          <div className="key">x</div>
+          <div className="key orange">x</div>
           <div className="key">4</div>
           <div className="key">5</div>
           <div className="key">6</div>
-          <div className="key">-</div>
+          <div className="key orange">-</div>
           <div className="key">1</div>
           <div className="key">2</div>
           <div className="key">3</div>
-          <div className="key">+</div>
-          <div className="key">0</div>
+          <div className="key orange">+</div>
+          <div className="key zero">0</div>
           <div className="key">.</div>
-          <div className="key">=</div>
+          <div className="key orange">=</div>
         </div>
 
       </div>
