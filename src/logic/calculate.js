f8ed50b22d3386bbbe6e649075f27a9d00d6d014
pountzas <nikos@pountzas.gr>
2021-08-20 00:04:48 +0300

    fix broken files provided by microverse

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -29,9 +29,9 @@ export default function calculate(obj, buttonName) {
     // If there is an operation, update next
     if (obj.operation) {
       if (obj.next) {
-        return { next: obj.next + buttonName };
+        return { ...obj, next: obj.next + buttonName };
       }
-      return { next: buttonName };
+      return { ...obj, next: buttonName };
     }
     // If there is no operation, update next and clear the value
     if (obj.next) {
@@ -49,9 +49,9 @@ export default function calculate(obj, buttonName) {
   if (buttonName === '.') {
     if (obj.next) {
       if (obj.next.includes('.')) {
-        return {};
+        return { ...obj };
       }
-      return { next: obj.next + '.' };
+      return { ...obj, next: `${obj.next}.` };
     }
     if (obj.operation) {
       return { next: '0.' };
@@ -60,7 +60,7 @@ export default function calculate(obj, buttonName) {
       if (obj.total.includes('.')) {
         return {};
       }
-      return { total: obj.total + '.' };
+      return { total: `${obj.total}.` };
     }
     return { total: '0.' };
   }
@@ -72,10 +72,9 @@ export default function calculate(obj, buttonName) {
         next: null,
         operation: null,
       };
-    } else {
-      // '=' with no operation, nothing to do
-      return {};
     }
+    // '=' with no operation, nothing to do
+    return {};
   }
 
   if (buttonName === '+/-') {
@@ -93,11 +92,20 @@ export default function calculate(obj, buttonName) {
   // When the user presses an operation button without having entered
   // a number first, do nothing.
   // if (!obj.next && !obj.total) {
-  //   return {};
+  //  return {};
   // }
+
+  // User pressed an operation after pressing '='
+  if (!obj.next && obj.total && !obj.operation) {
+    return { ...obj, operation: buttonName };
+  }
 
   // User pressed an operation button and there is an existing operation
   if (obj.operation) {
+    if (obj.total && !obj.next) {
+      return { ...obj, operation: buttonName };
+    }
+
     return {
       total: operate(obj.total, obj.next, obj.operation),
       next: null,
