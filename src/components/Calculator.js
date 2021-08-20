9a48b4eef5d99fe975928b9c92e2621079610267
pountzas <nikos@pountzas.gr>
2021-08-21 00:18:11 +0300

    remove this. from Calculator's return

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -34,42 +34,42 @@ const Calculator = () => {
     return `${displayTotal} ${displayOperation} ${displayNext}`;
   }
 
-  render() {
-    return (
-      <div className="calculator">
-        <div className="display">
-          {this.updateDisplay()}
-        </div>
-
-        <div className="keyboard">
-          <button type="button" className="key" onClick={() => { this.onClickHandler('AC'); }}>AC</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('+/-'); }}>+/-</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('%'); }}>%</button>
-          <button type="button" className="key orange" onClick={() => { this.onClickHandler('÷'); }}>÷</button>
-
-          <button type="button" className="key" onClick={() => { this.onClickHandler('7'); }}>7</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('8'); }}>8</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('9'); }}>9</button>
-          <button type="button" className="key orange" onClick={() => { this.onClickHandler('x'); }}>x</button>
-
-          <button type="button" className="key" onClick={() => { this.onClickHandler('4'); }}>4</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('5'); }}>5</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('6'); }}>6</button>
-          <button type="button" className="key orange" onClick={() => { this.onClickHandler('-'); }}>-</button>
-
-          <button type="button" className="key" onClick={() => { this.onClickHandler('1'); }}>1</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('2'); }}>2</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('3'); }}>3</button>
-          <button type="button" className="key orange" onClick={() => { this.onClickHandler('+'); }}>+</button>
-
-          <button type="button" className="key zero" onClick={() => { this.onClickHandler('0'); }}>0</button>
-          <button type="button" className="key" onClick={() => { this.onClickHandler('.'); }}>.</button>
-          <button type="button" className="key orange" onClick={() => { this.onClickHandler('='); }}>=</button>
-        </div>
-
+  // render() {
+  return (
+    <div className="calculator">
+      <div className="display">
+        {updateDisplay()}
       </div>
-    );
-  }
-}
+
+      <div className="keyboard">
+        <button type="button" className="key" onClick={() => { onClickHandler('AC'); }}>AC</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('+/-'); }}>+/-</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('%'); }}>%</button>
+        <button type="button" className="key orange" onClick={() => { onClickHandler('÷'); }}>÷</button>
+
+        <button type="button" className="key" onClick={() => { onClickHandler('7'); }}>7</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('8'); }}>8</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('9'); }}>9</button>
+        <button type="button" className="key orange" onClick={() => { onClickHandler('x'); }}>x</button>
+
+        <button type="button" className="key" onClick={() => { onClickHandler('4'); }}>4</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('5'); }}>5</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('6'); }}>6</button>
+        <button type="button" className="key orange" onClick={() => { onClickHandler('-'); }}>-</button>
+
+        <button type="button" className="key" onClick={() => { onClickHandler('1'); }}>1</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('2'); }}>2</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('3'); }}>3</button>
+        <button type="button" className="key orange" onClick={() => { onClickHandler('+'); }}>+</button>
+
+        <button type="button" className="key zero" onClick={() => { onClickHandler('0'); }}>0</button>
+        <button type="button" className="key" onClick={() => { onClickHandler('.'); }}>.</button>
+        <button type="button" className="key orange" onClick={() => { onClickHandler('='); }}>=</button>
+      </div>
+
+    </div>
+  );
+};
+// }
 
 export default Calculator;
