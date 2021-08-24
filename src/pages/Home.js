e502a8272e8e64186b780bd401609b7301101fa6
pountzas <nikos@pountzas.gr>
2021-08-24 22:01:12 +0300

    fix problem not rendering Home arrow function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const Home = () => (
   <>
     <h2>Welcome Math Magicians!</h2>
     <p>
@@ -15,7 +15,7 @@ const Home = () => {
       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
       in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
     </p>
-  </>;
-};
+  </>
+);
 
 export default Home;
