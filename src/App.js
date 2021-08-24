df4d9476bda60eaede973b9e01076e219804d140
pountzas <nikos@pountzas.gr>
2021-08-24 20:19:28 +0300

    import react DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
