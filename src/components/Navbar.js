10ecfc956bea68e3966a44f0fc4132e434674592
pountzas <nikos@pountzas.gr>
2021-08-24 21:58:39 +0300

    add navbar es6 arrow function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,23 @@
 /* eslint-disable react/prefer-stateless-function */
 // import React, { Component } from 'react';
 
-// class Navbar extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>hello</h1>
-//       </div>
-//     );
-//   }
-// }
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
 
 // export default Navbar;
